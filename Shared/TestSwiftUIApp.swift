ecfd112d30138b5a9784d07a8459f6862e797d3c
Erxhan Selmani <erxhan@tooddoc.com>
2021-02-05 20:06:15 +0100

    Initial Commit

---
+++ Shared/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  Shared
+//
+//  Created by Erxhan Selmani on 05/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
